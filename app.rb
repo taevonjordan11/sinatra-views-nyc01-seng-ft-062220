a9cf69b6c89f3da385e58d857d5faad79c8acec4
Taevon Jordan <nude.plus@MacBook-Pro.home>
2020-07-13 17:55:17 -0400

    Done.

--- app.rb
+++ app.rb
@@ -3,6 +3,14 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
 	get '/' do
-		
+		erb :index
+	end
+
+	get '/info' do
+		erb :info
+	end
+
+	get '/person' do
+		erb :person
 	end
 end
